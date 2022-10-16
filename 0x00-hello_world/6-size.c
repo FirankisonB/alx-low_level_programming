069554e5a9c8e581b7b115a64555402ba9163037
FirankisonB <minde12@yahoo.com>
2022-10-16 15:29:18 -0700

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,7 +16,7 @@ int main(void)
 	printf("Size of a char: %c byte(s)\n", sizeof(a));
 	printf("Size of an int: %u byte(s)\n",  sizeof(b));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(c));
-	printf("Size of a lomg long int: %llu byte(s)\n", sizeof(d));
+	printf("Size of a long long int: %llu byte(s)\n", sizeof(d));
 	printf("Size of a float: %f byte(s)\n", sizeof(f));
 	return (0);
 }
