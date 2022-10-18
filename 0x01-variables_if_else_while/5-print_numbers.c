a8ad5daf3e23a8dd5622ed859f22eb9119fd887d
FirankisonB <minde12@yahoo.com>
2022-10-18 06:05:11 -0700

    task567

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ * Return: Always
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+
+	printf("\n");
+
+	return (0);
+}
+
