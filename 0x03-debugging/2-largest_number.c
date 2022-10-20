48328f6335b1ee0e7f0cbc1ef24ca2c771415ccb
FirankisonB <minde12@yahoo.com>
2022-10-20 07:12:58 -0700

    Task2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,22 +12,18 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b)
+	if (a > b && b > c)
 	{
-		if (b > c)
-			largest = a;
-		if else(a > c)
-			largest = a;
-		else
-			largest = c;
-
+		largest = a;
+	}
+	else if (b > a && a > c)
+	{
+		largest = b;
 	}
 	else
 	{
-		if (b < c)
-			largest = c;
-		else
-			largest = b;
+		largest = c;
 	}
+
 	return (largest);
 }
