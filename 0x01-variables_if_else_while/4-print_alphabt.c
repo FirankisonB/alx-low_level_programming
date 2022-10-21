93f5b5da1df4d760ada63de92166dbf1baaff89a
FirankisonB <minde12@yahoo.com>
2022-10-21 06:53:16 -0700

    Task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,13 +6,16 @@
  */
 int main(void)
 {
-	char la;
+	char c;
 
-	for (la = 'a'; la <= 'z'; la++)
+	c = 'a';
+	while (c <= 'z')
 	{
-		if (la != 'e' && la != 'q')
-			putchar(la);
+		if ((c != 'e' && c != 'q') && c <= 'z')
+			putchar(c);
+		c++;
 	}
+	putchar('\n');
 
 	return (0);
 
