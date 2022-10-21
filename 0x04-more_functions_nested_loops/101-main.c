f08e5d3512b5b802a1b12e22a828e0a4aff1448b
FirankisonB <minde12@yahoo.com>
2022-10-21 04:20:53 -0700

    T12

--- 0x04-more_functions_nested_loops/101-main.c
+++ 0x04-more_functions_nested_loops/101-main.c
@@ -7,15 +7,17 @@
  */
 int main(void)
 {
-	print_number(98);
+	print_number(-1);
 	_putchar('\n');
-	print_number(402);
+	print_number(-4096);
 	_putchar('\n');
 	print_number(1024);
 	_putchar('\n');
 	print_number(0);
 	_putchar('\n');
-	print_number(-98);
+	print_number(-99);
+	_putchar('\n');
+	print_number(-2147483648);
 	_putchar('\n');
 	return (0);
 }
