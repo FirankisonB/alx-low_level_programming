e0b027b8664339bf5952c89130068a6eeeb8593f
FirankisonB <minde12@yahoo.com>
2022-10-21 05:41:52 -0700

    T10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of two different numbers
+ *
+ * Return: Always 0 (success)
+ *
+ */
+int main(void)
+{
+	int ones = '0';
+	int tens = '0';
+
+	for (tens = '0'; tens <= '9'; tens++)/* prints tens digit*/
+	{
+		for (ones = '0'; ones <= '9'; ones++)/* prints ones digit*/
+		{
+			if (!((ones == tens) || (tens > ones))/* eliminates repition*/
+			{
+			putchar(tens);
+			putchar(ones);
+				if (!(ones == '9' && tens == '8'))/*adds comma and space*/
+				{
+				putchar(',');
+				putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
