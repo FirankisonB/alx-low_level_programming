eb84c2f5fd1c53e967c97ad3a23f2d52b209cc0f
FirankisonB <minde12@yahoo.com>
2022-10-24 14:26:54 -0700

    Mandatory tasks

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,14 +1,13 @@
 #include "main.h"
 
 /**
- * main - check the code.
+ *print_times_table - check the code
  *
  * Return: Always 0.
  */
 void print_times_table(int n)
 {
-	
 	if (n > 15 || < 0)
 		printf("Nothing\n");
-       	return (0);
+	return (0);
 }
